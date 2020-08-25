aeaae5d85297a27460cba11c29a386ddc6e125eb
Toshi <waykiyoharu@gmail.com>
2020-08-25 14:33:17 +0900

    strageからコピペした内容を修正し、fileupload用のuploadsフォルダを作成

--- routes/profile.js
+++ routes/profile.js
@@ -2,17 +2,17 @@ const express = require('express');
 const router = express.Router();
 const multer = requier('multer');
 
-var storage = multer.diskStorage({
+const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, '/tmp/my-uploads')
+      cb(null, '')
     },
     filename: function (req, file, cb) {
       const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
       cb(null, file.fieldname + '-' + uniqueSuffix)
     }
-  })
+  });
   
-  var upload = multer({ storage: storage })
+  const upload = multer({ storage: storage })
 
 
 
