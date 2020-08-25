5b64edd39bf9fc252b569f092fa0b96a04287b75
Toshi <waykiyoharu@gmail.com>
2020-08-25 14:59:15 +0900

    requireの綴りが違ったので修正

--- routes/profile.js
+++ routes/profile.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const multer = requier('multer');
+const multer = require('multer');
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
